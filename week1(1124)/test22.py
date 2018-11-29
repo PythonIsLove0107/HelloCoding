53ebba9388c1c149bb6eaf3eb1ee3700cd12049e
PythonIsLove0107 <PythonIsLove0107@gmail.com>
2018-11-29 16:07:10 +0900

    테스트중...git 공부중

--- week1(1124)/test22.py
+++ week1(1124)/test22.py
@@ -4,9 +4,7 @@
 #max
 
 print("divmod(5, 2)->" , divmod(5, 2))
-# 수치형 자료형 x, y에 대해 x의 y승을 반환하는 함수
-# 2의 3승, 즉, 8을 출력
-print("pow(2, 3)->" , pow(2, 3))
+print("pow(2, 3)->" , pow(2, 3)) # 수치형 자료형 x, y에 대해 x의 y승을 반환하는 함수 //// 2의 3승, 즉, 8을 출력
 print("chr(97)->", chr(97)) # 아스키 코드값이 97인 문자 -> a
 print(pow(2, 3))
 print("max('python')->", max("python"))
@@ -14,6 +12,7 @@ a = [1,       2,3                ]
 print(type(a))
 print(len((a)))
 print(type(str(a)))
+print(str(a))
 print(len((str(a)))) # 왜 이렇게 되지? #####################question
 
 #1 2 3 4 5 6 7 8 9
